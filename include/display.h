95a337be5f13ea4555e49af4ba878147ed715278
Jonas Arnold <jonas.arnold@live.de>
2022-03-10 15:55:29 +0100

    switched library for display and fixed display code

--- include/display.h
+++ include/display.h
@@ -1,8 +1,7 @@
 #include <Arduino.h>
 #include <SPI.h>
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+#include <Wire.h>               // Only needed for Arduino 1.6.5 and earlier
+#include "SSD1306Wire.h"        // legacy: #include "SSD1306.h"
 
 // declaration
 void Init_Display();
--- src/display.cpp
+++ src/display.cpp
@@ -1,41 +1,30 @@
 #include "display.h"
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
-// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-
+// Initialize the OLED display using Arduino Wire:
+SSD1306Wire display(0x3c, 5, 4);
 
 // init
 void Init_Display()
 {
-    // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-    if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
-    }
+  // Initialising the UI will init the display too.
+  display.init();
 
-    // Show initial display buffer contents on the screen --
-    // the library initializes this with an Adafruit splash screen.
-    display.display();
-    delay(2000); // Pause for 2 seconds
-    
-    // Clear the buffer
-    display.clearDisplay();
+  display.flipScreenVertically();
+  display.setFont(ArialMT_Plain_10);
 
-    Serial.println("Display initialized.");
+  Serial.println("Display initialized.");
 }
 
 // show text
 void ShowText_Display(String text)
 {
-    display.clearDisplay();
-    display.setTextSize(2); // Draw 2X-scale text
-    display.setTextColor(WHITE);
-    display.setCursor(10, 0);
-    display.println(text);
-    display.display();      // Show initial text
-    delay(100);
+  display.clear();
+
+  display.setColor(WHITE);
+  display.setTextAlignment(TEXT_ALIGN_CENTER);
+  display.drawString(64, 15, text);
+  display.setFont(ArialMT_Plain_24);
+
+  display.display();
 }
