6cb688d7185f798308a9a5b5c9f49fb2a097cb0d
FloArnold <florian-arnold@hotmail.com>
2022-03-24 11:29:33 +0100

    i2c_communication

--- src/i2c.cpp
+++ src/i2c.cpp
@@ -1,21 +1,17 @@
-#include <Arduino.h>
-#include <Wire.h>                   // Bibliothek, die Funktionen für die I2C-Kommunikation enthält
+//#include <Arduino.h>
+//#include <Wire.h>
 #include <WireSlave.h>
 #include "i2c.h"
 
-//#define SLAVE_ADDRESS 0x01          // I2C Adresse vom Slave (ESP32)
 #define I2C_SDA 25
 #define I2C_SCD 26
-#define I2C_SLAVE_ADDR 0x04
+#define I2C_SLAVE_ADDR 0x04           // I2C Adresse vom Slave (ESP32)
 
 // vars
-int command = 0;                    // Befehl; no_command = 0 / speed_up = 1 / speed_down = 2 / stopp = 3
+int command = 0;                      // Befehl; no_command = 0 / speed_up = 1 / speed_down = 2 / stopp = 3
 byte state;
 byte batteryLevel;
 byte speed;
-
-void requestEvent();
-void receiveEvent(int howMany);
 
 void I2C_Init() {
 
@@ -25,9 +21,8 @@ void I2C_Init() {
       while(1) delay(100);
   }
 
-  WireSlave.onRequest(requestEvent);
-  WireSlave.onReceive(receiveEvent);
-
+  WireSlave.onRequest(sendData);
+  WireSlave.onReceive(receiveData);
 }
 
 // function that runs whenever the master sends an empty packet.
@@ -35,76 +30,59 @@ void I2C_Init() {
 // do not perform time-consuming tasks inside this function,
 // do them elsewhere and simply read the data you wish to
 // send inside here.
-void requestEvent()
-{
-    static byte y = 0;
+void sendData() {
 
-    WireSlave.print("y is ");
-    WireSlave.write(y++);
+    byte data [] = {state, batteryLevel, speed};
+    WireSlave.write(data, 3);
+    WireSlave.flush();
 }
-
 
 // function that executes whenever a complete and valid packet
 // is received from master
 // this function is registered as an event, see setup()
-void receiveEvent(int howMany)
-{
-    while (1 < WireSlave.available()) // loop through all but the last byte
-    {
-        char c = WireSlave.read();  // receive byte as a character
-        Serial.print(c);            // print the character
+void receiveData(int howMany) {
+
+    if(WireSlave.available()) {
+        command = WireSlave.read();
+        WireSlave.flush();
     }
-
-    int x = WireSlave.read();   // receive byte as an integer
-    Serial.println(x);          // print the integer
 }
 
-/*
-void receiveData(int byteCount) {
+int I2C_Handle() {                    // Befehl; no_command = 0 / speed_up = 1 / speed_down = 2 / stopp = 3
 
-  if(comWire.available()) {
-    command = comWire.read();
-    comWire.flush();
-  }
-}
+    WireSlave.update();
 
-void sendData() {
-
-  byte data [] = {state, batteryLevel, speed};
-  comWire.write(data, 3);
-  comWire.flush();
-}
-*/
-
-int I2C_Handle() {                  // Befehl; no_command = 0 / speed_up = 1 / speed_down = 2 / stopp = 3
-
-  WireSlave.update();
-
-  if(command!=0) {
-    int x = command;
-    command = 0;
-    return x;
-  }
-  return command;
+    if(command!=0) {
+        int command_out = command;
+        command = 0;
+        return command_out;
+    }
+    return command;
 }
 
 int get_state() {
-  return state;
+
+    return state;
 }
 void set_state(int x) {
-  state = x;
+
+    state = x;
 }
 
 int get_batteryLevel() {
-  return batteryLevel;
+
+    return batteryLevel;
 }
 void set_batteryLevel(int x) {
-  batteryLevel = x;
+
+    batteryLevel = x;
 }
 
 int get_speed() {
-  return speed;
+
+    return speed;
 }
 void set_speed(int x) {
-  speed = x;
+
+    speed = x;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -35,11 +35,12 @@ void loop() {
   // handle all periodic stuff
   driveCommand = I2C_Handle();
   if(driveCommand) {
-    // /* transfer of command to drive controller */
-    Display_ShowText(15, 15, String(driveCommand));
+    /* transfer command immediately to drive controller */
+    Display_ShowText(15, 15, String("Current command: " + driveCommand));
   }
 
-  // Test_I2C START
+  /* update outgoing data (state / batteryLevel / speed) */
+  // Test_I2C
   set_state(get_state() + 1);
   set_batteryLevel(get_batteryLevel() + 1);
   set_speed(get_speed() + 1);
@@ -52,12 +53,11 @@ void loop() {
   sensorValue = CoilSensor_Read();
   Helpers_SerialPrintLnAndVal("Read sensor value: ", sensorValue);
 
-  // read poti
+  // read poti (motor test)
   potValueAnalog = analogRead(14);
   potValPct = map(potValueAnalog, 0, 4095, 0, 100);
   // set motor speed
-  Motors_Forward(potValPct);
-  
+  Motors_Forward(potValPct);  
 
   // update display
   Display_Clear();
@@ -65,8 +65,6 @@ void loop() {
   Display_ShowText(80, 15, String(sensorValue));
   Display_ShowText(15, 40, String("Motors pct:"));
   Display_ShowText(80, 40, String(potValPct));
-
-
 
   delay(50);
 }
