0b5cb45994ab71d561b7028ade7e86934f5699b7
Jonas Arnold <jonas.arnold@live.de>
2022-05-29 15:30:05 +0200

    update battery measurement, removed desktop environment

--- src/battery-monitoring.cpp
+++ src/battery-monitoring.cpp
@@ -2,24 +2,42 @@
 #include "battery-monitoring.h"
 #include "helpers.h"
 
+/* PINS */
 const int measuring_pin = 14;
 
-const double scalingFactor = 2.7447;  // = 1/(47/129)
-const double milliVoltsPerAnalogVal = 0.080566; // = (330/4096);
+/* CONSTANTS */
+const double scalingFactor = 2.7447;                // = 1/(47/129)
+const double milliVoltsPerAnalogVal = 0.080566;     // = (330/4096);
+const int millisBetweenBatteryMeasurement = 2000;   // milliseconds between battery measurement
 
+/* VARIABLES */
+static double batteryVoltage;       // stores battery voltage in millivolts
+static long lastBatteryMeasurement; // stores last battery measurement millis
 
 void BatteryMonitoring_Init()
 {
     pinMode(measuring_pin, INPUT);
+    batteryVoltage = 0;
+    lastBatteryMeasurement = 0;
 }
 
 /* returns battery voltage in millivolts  */
 double BatteryMonitoring_GetBatteryVoltage()
 {
-    double voltage = analogRead(measuring_pin) * milliVoltsPerAnalogVal; // get voltage in millivolts
-    voltage = voltage * scalingFactor;  // multiply by voltage divider constant
-    //Helpers_SerialPrintLnAndVal("Battery voltage:", (uint16_t)voltage);
-    return voltage; 
+    unsigned long now = millis();
+    // request battery voltage from sensor if measurement is overdue
+    if(now - lastBatteryMeasurement > millisBetweenBatteryMeasurement)
+    {
+        double voltage  = analogRead(measuring_pin) * milliVoltsPerAnalogVal; // get voltage in millivolts
+        voltage = voltage * scalingFactor; // multiply by voltage divider constant
+        Helpers_SerialPrintLnAndVal("Battery voltage measured:", (uint16_t)voltage);
+        batteryVoltage = voltage;  // set stored value
+
+        // set now as last measurement time 
+        lastBatteryMeasurement = now;
+    }
+
+    return batteryVoltage; 
 }
 
 uint8_t BatteryMonitoring_GetPercent()
